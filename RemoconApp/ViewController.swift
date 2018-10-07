e0e4ae9256046b07b45299db88ec231396a0cd20
ShunichiTanaka <tanaka-shu@gnavi.co.jp>
2018-10-07 17:06:42 +0900

    Initial Commit

---
+++ RemoconApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemoconApp
+//
+//  Created by 田中 俊一 on 2018/10/07.
+//  Copyright © 2018年 田中 俊一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
